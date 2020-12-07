2bdc25d0ed38dd9e9ffc3a523b286ece2f214c8b
AlejoLopezAvila <alejo.lopez@digilytics.ai>
2020-12-07 15:59:05 +0000

    Initial commit

---
+++ src/tools.py
@@ -0,0 +1,32 @@
+import torch
+
+def check_device():
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print('There are %d GPU(s) available.' % torch.cuda.device_count())
+        print('We will use the GPU:', torch.cuda.get_device_name(0))
+        print("Cuda architectures lis{}".format(torch.cuda.get_arch_list()))
+        print("Device capability {}".format(torch.cuda.get_device_capability()))
+    else:
+        print('No GPU available, using the CPU instead.')
+        device = torch.device("cpu")
+    return device
+
+
+def preprocess_data_BERT(data_path):
+    df = pd.read_csv(data_path, encoding="latin-1")
+    cols = df.columns.tolist()
+    df[cols[0]] = df[cols[0]].fillna(method="ffill")
+    # Encoding tags and pos
+    tag_enc = prep.LabelEncoder()
+    pos_enc = prep.LabelEncoder()
+
+    # We need to preserve the class LabelEncoder
+    df["POS"] = pos_enc.fit_transform(df["POS"])
+    df["Tag"] = tag_enc.fit_transform(df["Tag"])
+
+    # Covert into lists of lists and group by sentence
+    sentences = df.groupby(cols[0])["Word"].apply(list).values
+    pos = df.groupby(cols[0])["POS"].apply(list).values
+    tag = df.groupby(cols[0])["Tag"].apply(list).values
+    return sentences, pos, tag, pos_enc, tag_enc
